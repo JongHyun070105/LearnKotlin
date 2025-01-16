5258b72bf2bff769deda67aa789869a6f4946f79
JongHyun070105 <bobi09361@gmail.com>
2025-01-16 12:09:03 +0900

    Kotlin

---
+++ src/String templates.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var customers = 10 // 대기 손님 10명
+    println("고객이 $customers 명 있습니다.")
+    // 예상 값 : 10명
+
+    println("고객이 ${customers + 1} 명 있습니다.")
+    // 예상 값 : 11명
+}
---
+++ src/Variables.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val popcorn = 5 // 팝콘 상자 5개
+    val hotdog = 7 // 핫도그 7개
+    var customers = 10 // 대기 손님 10명
+
+    customers = 8 // 일부 대기 손님이 탈주
+    print(customers) // 남아있는 손님 출력
+}
