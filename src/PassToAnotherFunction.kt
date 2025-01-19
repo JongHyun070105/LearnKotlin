f7637b2c49700c549861e1cf2877d7fe240ea1f6
JongHyun070105 <bobi09361@gmail.com>
2025-01-19 15:14:11 +0900

    Kotlin

---
+++ src/PassToAnotherFunction.kt
@@ -0,0 +1,23 @@
+//fun main() {
+//    val numbers = listOf(1, -2, 3, -4, 5, -6)
+//
+//
+//    val positives = numbers.filter ({ x -> x > 0 })
+//
+//    val isNegative = { x: Int -> x < 0 }
+//    val negatives = numbers.filter(isNegative)
+//
+//    println(positives)
+//    println(negatives)
+//}
+
+//fun main() {
+//    val numbers = listOf(1, -2, 3, -4, 5, -6)
+//    val doubled = numbers.map { x -> x * 2 }
+//
+//    val isTripled = { x: Int -> x * 3 }
+//    val tripled = numbers.map(isTripled)
+//
+//    println(doubled)
+//    println(tripled)
+//}
