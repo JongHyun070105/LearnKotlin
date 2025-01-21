22c92e83803aaad86688e617ee6d53e5f5c6d2c1
JongHyun070105 <bobi09361@gmail.com>
2025-01-21 22:02:04 +0900

    Kotlin

--- src/CopyInstance﻿.kt
+++ src/CopyInstance﻿.kt
@@ -1,13 +1,13 @@
-data class User(val name: String, val id: Int)
-
-fun main() {
-    val user = User("Alex", 1)
-    val secondUser = User("Alex", 1)
-    val thirdUser = User("Max", 2)
-
-    println(user.copy())
-
-    println(user.copy("Max"))
-
-    println(user.copy(id = 3))
-}
+//data class User(val name: String, val id: Int)
+//
+//fun main() {
+//    val user = User("Alex", 1)
+//    val secondUser = User("Alex", 1)
+//    val thirdUser = User("Max", 2)
+//
+//    println(user.copy())
+//
+//    println(user.copy("Max"))
+//
+//    println(user.copy(id = 3))
+//}
--- src/Practice8.kt
+++ src/Practice8.kt
@@ -1,22 +1,44 @@
-import kotlin.random.Random
+//data class Employee(val name : String, var salary : Int)
+//
+//fun main() {
+//    val emp = Employee("Mary", 20)
+//    println(emp)
+//    emp.salary += 10
+//    println(emp)
+//}
 
-data class Employee(val name: String, var salary: Int)
+//data class Person(val name: Name, val address: Address, val ownsAPet: Boolean = true)
+//data class Name(val first : String, val last : String)
+//data class Address(val street: String, val city : City)
+//data class City(val name:String, val country : String)
+//
+//fun main() {
+//    val person = Person(
+//        Name("John", "Smith"),
+//        Address("123 Fake Street", City("Springfield", "US")),
+//        ownsAPet = false
+//    )
+//}
 
-class RandomEmployeeGenerator(var minSalary : Int, var maxSalary : Int) {
-    val names = listOf("종현","동균","시우","동석","태영","환희")
-    fun generateEmployee() : Employee{
-        return Employee(names.random(),
-            Random.nextInt(from = minSalary, until = maxSalary))
-    }
-}
-
-fun main() {
-    val empGen = RandomEmployeeGenerator(10, 30)
-    println(empGen.generateEmployee())
-    println(empGen.generateEmployee())
-    println(empGen.generateEmployee())
-    empGen.minSalary = 50
-    empGen.maxSalary = 100
-    println(empGen.generateEmployee())
-    println(empGen.generateEmployee())
-}
+//import kotlin.random.Random
+//
+//data class Employee(val name: String, var salary: Int)
+//
+//class RandomEmployeeGenerator(var minSalary : Int, var maxSalary : Int) {
+//    val names = listOf("종현","동균","시우","동석","태영","환희")
+//    fun generateEmployee() : Employee{
+//        return Employee(names.random(),
+//            Random.nextInt(from = minSalary, until = maxSalary))
+//    }
+//}
+//
+//fun main() {
+//    val empGen = RandomEmployeeGenerator(10, 30)
+//    println(empGen.generateEmployee())
+//    println(empGen.generateEmployee())
+//    println(empGen.generateEmployee())
+//    empGen.minSalary = 50
+//    empGen.maxSalary = 100
+//    println(empGen.generateEmployee())
+//    println(empGen.generateEmployee())
+//}
