dd95518ecf7c738a6e9bc592db487b93b4db9662
JongHyun070105 <bobi09361@gmail.com>
2025-01-23 21:45:56 +0900

    Kotlin

---
+++ src/UseSafeCalls﻿.kt
@@ -0,0 +1,12 @@
+//fun lengthString(maybeString: String?): Int? = maybeString?.length
+//
+//fun main() {
+//    val nullString: String? = null
+//    println(lengthString(nullString))
+//}
+//
+//fun main() {
+//    val nullString: String? = null
+//    println(nullString?.uppercase())
+//    // null
+//}
